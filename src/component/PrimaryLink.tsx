bed660a9627958ebef37719afc1028e9a452e5bb
Taylor Hoss <hosstay@gmail.com>
2024-06-13 16:49:11 -0500

    Dumpstered everything

--- src/component/PrimaryLink.tsx
+++
@@ -1,8 +0,0 @@
-import Link, {type LinkProps} from "next/link";
-import {type ReactNode} from "react";
-
-export function PrimaryLink(props: LinkProps & { children: ReactNode }) {
-  return <Link {...props} className={"hover:text-cyan-600"}>
-    {props.children}
-  </Link>
-}
